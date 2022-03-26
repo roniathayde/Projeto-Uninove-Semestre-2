df05d80e35905c91082b79b6b1bd36ca75738428
Roniathayde <pontes014@gmail.com>
2022-03-26 12:36:55 -0300

    Adicionando ferramenta de midia na página home

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ const body = document.querySelector("body"),
         toggle = body.querySelector(".toggle"),
         searchBtn = body.querySelector(".search-box"),
         modeSwitch = body.querySelector(".toggle-switch"),
-        modeText = body.querySelector(".mode-text");
+        modeText = body.querySelector(".mode-text"),
+        midias = body.querySelector(".midias");
+
+
 
         toggle.addEventListener("click", () =>{
             sidebar.classList.toggle("close")
+            toggle.classList.toggle("rotate")
         })
 
-
+        
         modeSwitch.addEventListener("click", () =>{
             body.classList.toggle("dark")
         })
+        
