61589de2a13a6533e8bf09d58421dee14a8ba433
Roniathayde <pontes014@gmail.com>
2022-04-15 13:37:51 -0300

    Corrigindo script midias

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const body = document.querySelector("body"),
         modeSwitch = body.querySelector(".toggle-switch"),
         modeText = body.querySelector(".mode-text"),
         midias = body.querySelector(".midias");
+var midias2 = document.querySelector(".open-midias");
+var icons = document.querySelectorAll(".icons-midia");
 
 
 
@@ -18,3 +20,30 @@ const body = document.querySelector("body"),
             body.classList.toggle("dark")
         })
         
+   
+
+        
+        midias2.addEventListener("click", ()=>{
+
+            if(icons[0].classList[1] == ("hide-share")){
+                for(var i = 0; i < icons.length; i++){
+                    icons[i].classList.remove("hide-share")
+                    icons[i].classList.add("display-share")
+                    
+                }
+            }else if(icons[0].classList[1] == ("display-share")){
+                for(var i = 0; i < icons.length; i++){
+                    icons[i].classList.remove("display-share")
+                    icons[i].classList.add("hide-share")
+                }
+            }
+           
+        })
+
+        if(icons[0].classList[0] == ("hide-share")){
+            
+            console.log("funfo")
+        }else if(icons[0].classList[0] == ("display-share")){
+
+        };
+
